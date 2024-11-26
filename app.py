979005963d6afba75d87f7484e71367421b0a967
ramzyathaya <91423678+ramzyathaya@users.noreply.github.com>
2024-11-26 21:58:11 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 import subprocess
+import shlex
+
+def run_command(cmd):
+    safe_cmd = shlex.split(cmd)
+    subprocess.call(safe_cmd)
+
+
+
+
+
 
 def greet(name):
     print(f"Hello, {name}!")
@@ -9,6 +19,5 @@ def run_command(cmd):
 if __name__ == "__main__":
     name = input("Enter your name: ")
     greet(name)
-
     cmd = input("Enter a command to run: ")
     run_command(cmd)
